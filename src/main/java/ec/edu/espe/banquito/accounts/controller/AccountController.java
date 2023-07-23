e77cd01cb7e3d762d617a0d30f523db0f6dcf37d
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-07-22 21:11:58 -0500

    AccountController Cross origin

--- src/main/java/ec/edu/espe/banquito/accounts/controller/AccountController.java
+++ src/main/java/ec/edu/espe/banquito/accounts/controller/AccountController.java
@@ -4,16 +4,14 @@ import ec.edu.espe.banquito.accounts.controller.res.AccountResDto;
 import ec.edu.espe.banquito.accounts.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/accounts")
+@CrossOrigin
 public class AccountController {
     private final AccountService accountService;
 
